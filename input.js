cc755a233b1f9cb9ca40846de79cbee9e317ea62
marlex1313 <50080369+marlex1313@users.noreply.github.com>
2022-02-17 22:54:28 +0100

    Update input.js

--- input.js
+++ input.js
@@ -12,3 +12,26 @@ document.addEventListener('mousedown', () => {
 document.addEventListener('mouseup', () => {
     camera.mouseUp();
 })
+
+var zoom = 1;
+var zoomX = 0, zoomY = 0;
+
+document.addEventListener('wheel', (e) =>{
+    if(e.deltaY < 0 && zoom < 4) {
+        zoomX -= canvas.width*zoom*0.5;
+        zoomY -= canvas.height*zoom*0.5;
+        zoom = zoom*2;
+    }
+    if(e.deltaY > 0 && zoom > 0.2) {
+        zoom = zoom*0.5;
+        zoomX += canvas.width*zoom*0.5;
+        zoomY += canvas.height*zoom*0.5;
+    }
+})
+
+
+
+function createCursor() {
+    sceene.fillStyle = "violet"
+    sceene.fillRect(canvas.width*0.5*zoom - 50*zoom + zoomX,canvas.height*0.5*zoom - 50*zoom + zoomY,100*zoom,100*zoom);
+}
